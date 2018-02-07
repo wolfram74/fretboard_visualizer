21c900adb79edb9937308ab873b9b0d034392a39
wolfram74 <phaugen@gmail.com>
2018-02-06 17:24:20 -0700

    board set up to list elements as intended

---
+++ docs/scripts/components.js
@@ -0,0 +1,53 @@
+Vue.component('fretboard', {
+  template:`
+    <div>
+      hello
+      <ol class='strings'>
+        <string v-for='(stringArray, index) in boardArray'
+          v-bind:key=index
+          v-bind:stringArray=stringArray
+          v-bind:stringVal=index
+          >
+          </string>
+      </ol>
+    </div>
+  `,
+  data: function(){
+    var stringCount = 6;
+    var fretCount = 18
+    var boardArray = []
+    for(var string=0; string<stringCount; string++){
+      var stringArray = []
+      for(var fret=0; fret<fretCount; fret++){
+        stringArray.push(fret)
+      }
+      boardArray.push(stringArray)
+    }
+    return {boardArray}
+  }
+})
+
+Vue.component('string', {
+  props:['stringArray', 'stringVal'],
+  template:`
+  <li class='string'>
+    <ol class='frets'>
+      <fret v-for='(fretVal, index) in stringArray'
+      v-bind:key=index
+      v-bind:fretVal=fretVal
+      v-bind:stringVal=stringVal
+      >
+      </fret>
+    </ol>
+  </li>
+  `
+})
+
+Vue.component('fret', {
+  props:['fretVal', 'stringVal'],
+  template:`
+  <li class='fret'>
+    S-{{stringVal}}-{{fretVal}}
+  </li>
+  `
+})
